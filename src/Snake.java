739a0887e29807a861093fbec0c92995cbfc585e
alisonlange45 <alange@macalester.edu>
2020-10-16 09:53:27 -0500

    snake moves on screen

---
+++ src/Snake.java
@@ -0,0 +1,80 @@
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Rectangle;
+
+public class Snake extends Rectangle{
+    private double speed = 20;
+    private static double x;
+    private static double y;
+    private static double width = 10;
+    private static double height = 10;
+    private CanvasWindow canvas;
+
+/**
+ * Sets snake dimensions
+ */
+    public Snake(CanvasWindow canvas) {
+        super(x, y, width, height);
+        this.setFilled(true);
+        this.canvas = canvas;
+        x = canvas.getWidth()*.4;
+        y = canvas.getHeight()*0.9;
+        this.updatePosition();
+    }
+
+/**
+ * Moves the snake on the canvas by changing its direction
+ */
+    public void moveX(double speed) {
+            x += speed;
+            this.updatePosition();
+            if (x <= 0) {
+                x = 0;
+                this.updatePosition();
+            }
+            if (x + width >= canvas.getWidth()) {
+                x = canvas.getWidth() - width;
+                this.updatePosition();
+            }    
+    }
+    public void moveY(double speed) {
+        y += speed;
+            this.updatePosition();
+            if (y <= 0) {
+                y = 0;
+                this.updatePosition();
+            }
+            if (y + width >= canvas.getHeight()) {
+                y = canvas.getHeight() - width;
+                this.updatePosition();
+            } 
+    }
+
+/**
+ * Moves the snake left
+ */
+    public void moveLeft(){
+        moveX(-speed);
+    }
+
+/**
+ * Moves the snake right
+ */
+    public void moveRight(){
+        moveX(speed);
+    }
+
+    public void moveUp(){
+        moveY(-speed);
+    }
+
+    public void moveDown(){
+        moveY(speed);
+    }
+
+/**
+ * Updates the postion of the snake
+ */
+    public void updatePosition(){
+        this.setPosition(x, y);   
+    } 
+}
---
+++ src/snakeGame.java
@@ -0,0 +1,44 @@
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsText;
+import edu.macalester.graphics.events.Key;
+
+public class snakeGame {
+        private static final int CANVAS_WIDTH = 600;
+        private static final int CANVAS_HEIGHT = 800;
+        private static CanvasWindow canvas;
+        private Snake snake;
+        
+        public static void main(String[] args) {   
+            snakeGame game = new snakeGame(); 
+            game.createSnake();
+    }
+     
+    /**
+     * Main Snake game method that animates the canvas
+     */
+    public snakeGame() {
+        canvas = new CanvasWindow("Snake!", CANVAS_WIDTH, CANVAS_HEIGHT);
+        // canvas.animate(() -> {snake.move();
+        //                     });
+                           
+        canvas.onKeyDown(event -> {
+            if (event.getKey() == Key.LEFT_ARROW) {
+                snake.moveLeft();
+            }
+            if (event.getKey() == Key.RIGHT_ARROW) {
+                snake.moveRight();
+            }
+            if (event.getKey() == Key.UP_ARROW) {
+                snake.moveUp();
+            }
+            if (event.getKey() == Key.DOWN_ARROW) {
+                snake.moveDown();
+            }
+        });   
+    }
+    private void createSnake() {
+        snake = new Snake(canvas);
+        canvas.add(snake);
+        canvas.draw();
+    }
+}
