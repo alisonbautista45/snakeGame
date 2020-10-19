6e366910558473dc8d7064df00d7ec75c312fd16
Imane Chatri <70664538+ImaneChatri@users.noreply.github.com>
2020-10-18 20:16:57 -0500

    Update Collision.java

--- src/Collision.java
+++ src/Collision.java
@@ -8,7 +8,6 @@ public class Collision {
 
     private Snake snake;
     private Food food;
-    private CanvasWindow canvas;
 
     // private List<Point> snakeHead;
 
@@ -21,8 +20,7 @@ public class Collision {
      * 
      */
 
-    public Collision (CanvasWindow canvas, Snake snake, Food food) {
-        this.canvas = canvas;
+    public Collision (Snake snake, Food food) {
         this.snake = snake;
         this.food = food;
         // this.snakeHead = snake.updateHead();
@@ -50,18 +48,4 @@ public class Collision {
     //     return false;
     // }
 
-
-     /**
-     * checks if the given position has collided with a wall.
-     * @param position the center of the snake's head.
-     * @return true if there was a collision, false if there wasn't one.
-     */
-    public boolean wallCollision(Point position) {
-        GraphicsObject collision = canvas.getElementAt(position);
-        if (collision instanceof Wall) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
