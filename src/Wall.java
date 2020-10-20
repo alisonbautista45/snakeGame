5967dd62d5fdfc108d806d82f5d0279fa5ccee4c
Imane Chatri <70664538+ImaneChatri@users.noreply.github.com>
2020-10-19 19:36:14 -0500

    Updated Wall color scheme

--- src/Wall.java
+++ src/Wall.java
@@ -7,11 +7,17 @@ import java.awt.Color;
 
 public class Wall extends Rectangle {
 
+    public static final Color YELLOW = new Color(238, 186, 76);
+    public static final Color RED = new Color(227, 73, 59);
+    public static final Color AQUA = new Color(35, 181, 175);
+    public static final Color LIGHT_AQUA = new Color(169, 221, 217);
+    public static final Color DARK_GREAY = new Color(58, 58, 60);
+
     public Wall(double x, double y, double width, double height) {
         super(x, y, width, height);
 
         Random random = new Random();
-        List<Color> colors = List.of(Color.GREEN, Color.BLUE, Color.CYAN, Color.ORANGE, Color.MAGENTA, Color.YELLOW);
+        List<Color> colors = List.of(YELLOW, RED, AQUA, LIGHT_AQUA);
         this.setFilled(true);
         this.setFillColor(colors.get(random.nextInt(colors.size())));
         this.setStroked(true);
